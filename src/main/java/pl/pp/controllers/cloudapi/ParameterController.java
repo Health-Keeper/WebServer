e95d25c45121c261b4576b1dc81f28006a70484d
DPP <dpeczek93@gmail.com>
2016-12-21 16:56:00 +0100

    Controller for getting all parameters

--- src/main/java/pl/pp/controllers/cloudapi/ParameterController.java
+++ src/main/java/pl/pp/controllers/cloudapi/ParameterController.java
@@ -1,7 +1,56 @@
 package pl.pp.controllers.cloudapi;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import pl.pp.controllers.MainPageController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by dpp on 12/18/16.
  */
+@Controller
 public class ParameterController {
+
+    private final Logger log = LoggerFactory.getLogger(MainPageController.class);
+
+    private final String dateTime = "measurement_date";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/parameter", method = RequestMethod.GET)
+    public String getParameter(@RequestParam(value="parameterType", required=false, defaultValue="systolic_press") String parameterType,
+                               Model model){
+        String result = restTemplate.getForObject("http://health-keeper-api.gear.host/api/Measurement/params/"
+                +dateTime+","+parameterType+"/1", String.class);
+        log.info(result);
+        List<String> dates = new ArrayList<>();
+        List<Double> parameters = new ArrayList<>();
+        result = result.replaceAll("\\\\", "");
+        result = result.substring(1, result.length()-1);
+        log.info(result);
+        JSONArray json = new JSONArray (result);
+        for (int i = 0; i < json.length(); i++) {
+            JSONObject j = json.getJSONObject(i);
+            parameters.add(Double.parseDouble(j.getString(parameterType)));
+            dates.add(j.get(dateTime).toString());
+            log.info(j.toString());
+        }
+        model.addAttribute("result", parameters);
+        return "chartPage";
+    }
+
+    //public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model)
+
 }
